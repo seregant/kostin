866220a5bae15f2b8972f9ee04290b099a213e5f
renfaizal <ren.faizal@gmail.com>
2018-11-09 19:09:27 +0700

    Update test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo $_SERVER['HTTP_REFERER']."<br>";
-	if (strpos($_SERVER['HTTP_REFERER'], 'category=view&module=booking') !== false) {
-    	echo 'true';
-	} else {
-		echo "kampret";
-	}
-?>
