e6c01806d49c32e1e2fb28bcf94982f260c749fd
renfaizal <ren.faizal@gmail.com>
2018-12-03 14:21:14 +0700

    fixing mailer css

--- admin/module/send_mail.php
+++ admin/module/send_mail.php
@@ -6,6 +6,8 @@
 
 	function sendMailTagihan($type, $id){
 		include 'config/database.php';
+		include 'css.php';
+
 		if ($type == 'booking') {
 			$sql = "select * from kostin_booking where book_id='".$id."'";
 			$sql2 = "select * from kostin_tagihan_booking where book_id='".$id."'";
@@ -48,7 +50,9 @@
 					    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 						<!-- Custom CSS -->
-						<link href="http://project.konco.online/admin/template/css/mail-booking.css" rel="stylesheet">
+						<style>
+							'.$invoice_css.'
+						</style>
 						<title></title>
 					</head>
 					<body>
@@ -181,9 +185,9 @@
 					   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 					    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 					    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-						<!-- Custom CSS -->
-						<link href="http://project.konco.online/admin/template/css/mail-booking.css" rel="stylesheet">
+						<style>
+							'.$invoice_css.'
+						</style>
 						<title></title>
 					</head>
 					<body>
@@ -274,6 +278,7 @@
 
 	function sendPaymentBookingSuccess($username, $password, $id){
 		include 'config/database.php';
+		include 'css.php';
 		
 		$sql = "select * from kostin_booking where book_id='".$id."'";
 		$sql2 = "select * from kostin_tagihan_booking where book_id='".$id."'";
@@ -318,7 +323,9 @@
 
 						<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-						<link href="http://project.konco.online/admin/template/css/mail.css" rel="stylesheet">
+						<style>
+							'.$payment_booking_css.'
+						</style>
 				<title></title>
 				</head>
 				<body>
